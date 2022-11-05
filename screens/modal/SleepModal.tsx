1da09d6cc2a29c80cf48302b2a5ddabee9afa81c
inderro1@students.zhaw.ch <inderro1@students.zhaw.ch>
2022-11-05 10:19:04 +0200

    created sleep modal, added star rating and started with time selection. todo: styling and wait for the date component

--- screens/modal/SleepModal.tsx
+++ screens/modal/SleepModal.tsx
@@ -1,9 +1,89 @@
-import {StyleSheet, View, Text} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
+import {DateTimePickerAndroid} from '@react-native-community/datetimepicker';
+import {RootStackScreenProps} from "../../types";
+import { Rating} from 'react-native-ratings';
 
-export default function SleepModal() {
+
+
+export default function SleepModal(navProps: RootStackScreenProps<"SleepModal">) {
+
+    const [newSleepTime, setNewSleepTime] = useState("");
+    const [time, setTime] = useState(new Date());
+    const [sleepRating, setSleepRating] = useState('')
+
+    // const onChange = (event: any, selectedDate: Date) => {
+    //     const currentDate = selectedDate;
+    //     setShow(false);
+    //     setDate(currentDate);
+    // };
+    //
+    //
+    // const showMode = (currentMode:any) => {
+    //         setShow(false);
+    //     setMode(currentMode);
+    // };
+
+    // const showDatepicker = () => {
+    //     showMode('date');
+    // };
+    //
+    // const showTimepicker = () => {
+    //     showMode('time');
+    // };
+
+    function saveSleepData(){
+        console.log("save")
+        navProps.navigation.navigate('Root');
+    }
+
+    function ratingCompleted(rating: string) {
+        console.log("Rating is: " + rating)
+        setSleepRating(rating);
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Add sleepModal stuff</Text>
+            <View>
+                <Text style={styles.title}>Bed Time: </Text>
+                {/*see the example from github webpage and after use the compontent, marco is doing */}
+                {/*<DateTimePicker*/}
+                {/*    mode="time"*/}
+                {/*    value={time}*/}
+                {/*    style={{width: 300, opacity: 1, height: 30, marginTop: 50}}*/}
+                {/*    onChange={onTimeChange}*/}
+                {/*/>*/}
+                {/*<Button onPress={showTimepicker} title="Show time picker!" />*/}
+                <Button title={'placeholder'}/>
+                <Text>selected: {Date.toLocaleString()}</Text>
+                {/*{show && (*/}
+                {/*    <DateTimePicker*/}
+                {/*        testID="dateTimePicker"*/}
+                {/*        value={date}*/}
+                {/*        mode={mode}*/}
+                {/*        is24Hour={true}*/}
+                {/*        onChange={onChange}*/}
+                {/*    />*/}
+                {/*)}*/}
+            </View>
+            <View >
+                <Text style={styles.title}> Wake up: </Text>
+                <TextInput></TextInput>
+            </View>
+            <View>
+                <Text style={styles.title}> Quality: </Text>
+                <Rating
+                    type='star'
+                    ratingCount={5}
+                    imageSize={40}
+                    onFinishRating={ratingCompleted}
+                    style={{ paddingVertical: 10 }}
+                />
+
+            </View>
+            <View>
+                <Button onPress={saveSleepData} title="Save"/>
+            </View>
         </View>
     );
 }
