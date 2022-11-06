140fd793454ee6cb50772382cada2ea42d59c7fb
prescherm <m.prescher@gantner-instruments.com>
2022-11-06 02:34:29 +0200

    some code improvements

--- screens/modal/StatusModal.tsx
+++ screens/modal/StatusModal.tsx
@@ -3,14 +3,14 @@ import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
 import {
     faFaceAngry,
-    faFaceLaughBeam, faFaceMehBlank,
-    faFaceRollingEyes, faFaceSmileBeam,
+    faFaceLaughBeam,
+    faFaceRollingEyes,
+    faFaceSmileBeam,
     faFaceSurprise,
     faFaceTired
 } from "@fortawesome/free-regular-svg-icons";
 import {faCloudShowersHeavy} from "@fortawesome/free-solid-svg-icons";
 import {MentalState} from "../../domain/MentalState";
-import {useEffect, useState} from "react";
 import {SleepData} from "../../domain/SleepData";
 import {EmotionData} from "../../domain/EmotionData";
 import {ExposureData} from "../../domain/ExposureData";
@@ -23,69 +23,58 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
 
     let mentalState = MentalState.getInstance();
 
-    const [emotionalData, setEmotionalData] = useState(new EmotionData(0, 0, 0, 0, 0, 0, 0, 0, 0));
-    const [sleepData, setSleepData] = useState(new SleepData(new Date(), new Date(), 0));
-    const [exposureData, setExposureData] = useState(new ExposureData(0, 0))
+    let averageEmotion: EmotionData = new EmotionData(0, 0, 0, 0, 0, 0, 0, 0, 0);
+    let averageExposure: ExposureData = new ExposureData(0, 0);
+    let averageSleep: SleepData = new SleepData(new Date(), new Date(), 0);
 
-    useEffect(() => {
-        let averageEmotion: EmotionData = new EmotionData(0, 0, 0, 0, 0, 0, 0, 0, 0);
-        let emotionDataCount = 0;
-        let averageExposure: ExposureData = new ExposureData(0, 0);
-        let exposureDataCount = 0;
-        let averageSleep: SleepData = new SleepData(new Date(), new Date(), 0);
-        let sleepDataCount = 0;
+    let emotionDataCount = 0;
+    let exposureDataCount = 0;
+    let sleepDataCount = 0;
 
-        for (let i = 0; i < mentalState.days.length; i++) {
-            let day = mentalState.days[i];
-            if (day.emotionData != null) {
-                console.log("DepressionLevel original:", day.emotionData.depressionLevel);
-                averageEmotion.anxietyLevel += +day.emotionData.anxietyLevel;
-                averageEmotion.angerLevel += +day.emotionData.angerLevel;
-                averageEmotion.happinessLevel += +day.emotionData.happinessLevel;
-                averageEmotion.fearLevel += +day.emotionData.fearLevel;
-                averageEmotion.distressLevel += +day.emotionData.distressLevel;
-                averageEmotion.disgustLevel += +day.emotionData.disgustLevel;
-                averageEmotion.calmnessLevel += +day.emotionData.calmnessLevel;
-                averageEmotion.overwhelmLevel += +day.emotionData.overwhelmLevel;
-                averageEmotion.depressionLevel += +day.emotionData.depressionLevel;
-                emotionDataCount++;
-            }
-            if (day.exposureData != null) {
-                averageExposure.disasterLevel += +day.exposureData.disasterLevel;
-                averageExposure.pollutionLevel += +day.exposureData.pollutionLevel;
-                exposureDataCount++;
-            }
-            if (day.sleepData != null) {
-                averageSleep.quality += +day.sleepData.quality;
-                sleepDataCount++;
-            }
+    for (let i = 0; i < mentalState.days.length; i++) {
+        let day = mentalState.days[i];
 
-            console.log("DepressionLevel", averageEmotion.depressionLevel);
-            if (emotionDataCount > 0) {
-                averageEmotion.anxietyLevel /= emotionDataCount;
-                averageEmotion.angerLevel /= emotionDataCount;
-                averageEmotion.happinessLevel /= emotionDataCount;
-                averageEmotion.fearLevel /= emotionDataCount;
-                averageEmotion.distressLevel /= emotionDataCount;
-                averageEmotion.disgustLevel /= emotionDataCount;
-                averageEmotion.calmnessLevel /= emotionDataCount;
-                averageEmotion.overwhelmLevel /= emotionDataCount;
-                averageEmotion.depressionLevel /= emotionDataCount;
-            }
-
-            if (exposureDataCount > 0) {
-                averageExposure.disasterLevel /= exposureDataCount;
-                averageExposure.pollutionLevel /= exposureDataCount;
-            }
-
-            if (sleepDataCount > 0) {
-                averageSleep.quality /= sleepDataCount;
-            }
-            setEmotionalData(averageEmotion);
-            setExposureData(averageExposure);
-            setSleepData(averageSleep);
+        if (day.emotionData != null) {
+            averageEmotion.anxietyLevel += +day.emotionData.anxietyLevel;
+            averageEmotion.angerLevel += +day.emotionData.angerLevel;
+            averageEmotion.happinessLevel += +day.emotionData.happinessLevel;
+            averageEmotion.fearLevel += +day.emotionData.fearLevel;
+            averageEmotion.distressLevel += +day.emotionData.distressLevel;
+            averageEmotion.disgustLevel += +day.emotionData.disgustLevel;
+            averageEmotion.calmnessLevel += +day.emotionData.calmnessLevel;
+            averageEmotion.overwhelmLevel += +day.emotionData.overwhelmLevel;
+            averageEmotion.depressionLevel += +day.emotionData.depressionLevel;
+            emotionDataCount++;
         }
-    }, []);
+        if (day.exposureData != null) {
+            averageExposure.disasterLevel += +day.exposureData.disasterLevel;
+            averageExposure.pollutionLevel += +day.exposureData.pollutionLevel;
+            exposureDataCount++;
+        }
+        if (day.sleepData != null) {
+            averageSleep.quality += +day.sleepData.quality;
+            sleepDataCount++;
+        }
+
+        if (emotionDataCount > 0) {
+            averageEmotion.anxietyLevel /= emotionDataCount;
+            averageEmotion.angerLevel /= emotionDataCount;
+            averageEmotion.happinessLevel /= emotionDataCount;
+            averageEmotion.fearLevel /= emotionDataCount;
+            averageEmotion.distressLevel /= emotionDataCount;
+            averageEmotion.disgustLevel /= emotionDataCount;
+            averageEmotion.calmnessLevel /= emotionDataCount;
+            averageEmotion.overwhelmLevel /= emotionDataCount;
+            averageEmotion.depressionLevel /= emotionDataCount;
+        }
+        if (exposureDataCount > 0) {
+            averageExposure.disasterLevel /= exposureDataCount;
+            averageExposure.pollutionLevel /= exposureDataCount;
+        }
+        if (sleepDataCount > 0) {
+            averageSleep.quality /= sleepDataCount;
+        }
+    }
 
     function shareResults() {
         console.log("Share it with your therapist, to do ")
@@ -105,7 +94,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Depression</Text>
-                            <Text style={styles.text}>{emotionalData.depressionLevel} %</Text>
+                            <Text style={styles.text}>{averageEmotion.depressionLevel.toFixed(2)} %</Text>
                         </View>
                         <View style={styles.innerInnerContainer}>
                             <FontAwesomeIcon
@@ -115,7 +104,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Anger</Text>
-                            <Text style={styles.text}>{emotionalData.angerLevel} %</Text>
+                            <Text style={styles.text}>{averageEmotion.angerLevel.toFixed(2)} %</Text>
                         </View>
                         <View style={styles.innerInnerContainer}>
                             <FontAwesomeIcon
@@ -125,7 +114,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Anxiety</Text>
-                            <Text style={styles.text}>{emotionalData.anxietyLevel} %</Text>
+                            <Text style={styles.text}>{averageEmotion.anxietyLevel.toFixed(2)} %</Text>
                         </View>
                         <View style={styles.innerInnerContainer}>
                             <FontAwesomeIcon
@@ -135,7 +124,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Happiness</Text>
-                            <Text style={styles.text}>{emotionalData.happinessLevel} %</Text>
+                            <Text style={styles.text}>{averageEmotion.happinessLevel.toFixed(2)} %</Text>
                         </View>
                     </View>
 
@@ -148,7 +137,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Fear</Text>
-                            <Text style={styles.text}>{emotionalData.fearLevel} %</Text>
+                            <Text style={styles.text}>{averageEmotion.fearLevel.toFixed(2)} %</Text>
                         </View>
 
                         <View style={styles.innerInnerContainer}>
@@ -159,7 +148,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Disgust</Text>
-                            <Text style={styles.text}>{emotionalData.disgustLevel} %</Text>
+                            <Text style={styles.text}>{averageEmotion.disgustLevel.toFixed(2)} %</Text>
                         </View>
                         <View style={styles.innerInnerContainer}>
                             <FontAwesomeIcon
@@ -170,9 +159,8 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                             />
 
                             <Text style={styles.text}>Overwhelm</Text>
-                            <Text style={styles.text}>{emotionalData.overwhelmLevel} %</Text>
+                            <Text style={styles.text}>{averageEmotion.overwhelmLevel.toFixed(2)} %</Text>
                         </View>
-
                     </View>
 
                     <Text style={styles.title}> Exposure </Text>
@@ -185,7 +173,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Pollution:</Text>
-                            <Text style={styles.text}>{exposureData.pollutionLevel} %</Text>
+                            <Text style={styles.text}>{averageExposure.pollutionLevel.toFixed(2)} %</Text>
                         </View>
                         <View style={styles.innerInnerContainer}>
                             <FontAwesomeIcon
@@ -195,7 +183,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Disasters:</Text>
-                            <Text style={styles.text}>{exposureData.disasterLevel} %</Text>
+                            <Text style={styles.text}>{averageExposure.disasterLevel.toFixed(2)} %</Text>
                         </View>
                     </View>
                     <Text style={styles.title}> Sleep </Text>
@@ -208,14 +196,12 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
                                 style={styles.icons}
                             />
                             <Text style={styles.text}>Sleep:</Text>
-                            <Text style={styles.text}>{sleepData.quality * 20} %</Text>
+                            <Text style={styles.text}>{(averageSleep.quality * 20).toFixed(2)} %</Text>
                         </View>
                     </View>
-
-
                 </View>
-
             </ScrollView>
+
             <TouchableOpacity
                 style={styles.saveButton}
                 onPress={shareResults}
@@ -225,6 +211,7 @@ export default function StatusModal(navProps: RootStackScreenProps<"StatusModal"
         </View>
     )
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -237,7 +224,6 @@ const styles = StyleSheet.create({
         alignContent: 'center',
         margin: 15,
         alignSelf: 'center',
-
     },
     innerContainer: {
         flexDirection: 'row',
@@ -245,11 +231,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignContent: 'center',
-        // marginLeft: 10,
-        // marginBottom: 20,
     },
     title: {
-        fontSize: 20,
+        marginLeft: 10,
+        marginTop: 10,
+        fontSize: 18,
         fontWeight: 'bold',
     },
     icons: {
@@ -272,7 +258,7 @@ const styles = StyleSheet.create({
         marginTop: 200,
         backgroundColor: "rgba(178,199,235,0.37)",
         position: "absolute",
-        top: 380,
+        top: 400,
         marginLeft: 20,
     },
 });
